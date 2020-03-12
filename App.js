e4e5492950287793701bbcf4a9ef994fce3b5b1d
hunter-graves <hgraves@mail.sfsu.edu>
2020-03-12 13:32:04 -0700

    converted inline styles to style sheet object for reusability and performance, cleaner JSX. Added a function to handle input

--- App.js
+++ App.js
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const[outputText, setOutputText] = useState('Open up App.js to start working on your App')
+  const[enteredGoal, setEnteredGoal] = useState('');
+
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
   return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+    <View style={Styles.screen}>
+      <View style={Styles.inputContainer}>
         <TextInput
          placeholder="Course Goal" 
-         style={{ width: 200, borderBottomColor: 'black', borderBottomWidth: 1 }}/>
+         style={Styles.input}
+         onChangeText={goalInputHandler}
+         />
         <Button title="ADD"/>
       </View>
     </View>
@@ -16,5 +24,17 @@ export default function App() {
 }
 
 const Styles = StyleSheet.create({
-
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  input: { 
+    width: 200, 
+    borderBottomColor: 'black', 
+    borderBottomWidth: 1 
+  }
 });
