574bf115795d5cd4556b35ad9925f9c4284efaef
hunter-graves <hgraves@mail.sfsu.edu>
2020-03-12 14:57:58 -0700

    import ScollView component, change my goal map's View wrapper into a ScrollView wrapper, to enable scrolling.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
   const[enteredGoal, setEnteredGoal] = useState('');
@@ -27,9 +27,9 @@ export default function App() {
          />
         <Button title="ADD" onPress={addGoalHandler}/>
       </View>
-      <View>
+      <ScrollView>
       {courseGoals.map((goal) => <View key={goal} style={Styles.listItem}><Text>{goal}</Text></View>)}
-      </View>
+      </ScrollView>
     </View>
   );
 }
